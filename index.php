14ced1eba841a8badf4f31b1bfcc8ade06275494
mastergoster <dugrais.julien@icloud.com>
2019-04-15 11:38:49 +0200

    first commit - html - css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>formulaire de connexion</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<section class="login-container">
+			<div>
+				<header>
+					<h2>Identification</h2>
+				</header>
+				<form action="" method="Post">
+					<input type="text" name="username" placeholder="Nom d'utilisateur" required="required" />
+					<input type="password" name="password" placeholder="Mot de passe" required="required" />
+					<button type="submit">Connexion</button>
+				</form>
+			</div>
+		</section>
+	</div>
+</body>
+</html>
