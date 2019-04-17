d5dfd1851ecc0d447ad220f95f3c02c30fd1fde9
mastergoster <dugrais.julien@icloud.com>
2019-04-17 15:02:30 +0200

    template html

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if(!empty($_POST)){
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>formulaire de connexion</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/form.css">
 </head>
 <body>
 	<div class="wrapper">
